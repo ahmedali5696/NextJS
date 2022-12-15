5a2c1f8b744969648176ab6b90f548e8df44f3a1
Ahmed Ali <ahmedali5696@outlook.com>
2022-12-15 09:38:15 +0200

    edit

--- hooks/useAddData.js
+++ hooks/useAddData.js
@@ -1,24 +1,28 @@
 import { getDatabase, ref, set } from "firebase/database";
 import { useDispatch } from "react-redux";
+import { useState } from "reaction "
 
 import { updateData } from "../store/dataSlice";
 
 
 export default function useAddData() {
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
 
 
   function addNewItem(path, item, id) {
     const db = getDatabase();
 
+    setLoading(true)
     set(ref(db, path + '/' + id), { ...item, id: id + 1 })
-      .then(() => {
+    	.then(() => {
         dispatch(updateData(path))
+        setLoading(false)
       })
       .catch((error) => {
         console.log(error)
       });
   }
 
-  return [addNewItem]
+  return [addNewItem, loading]
 }
