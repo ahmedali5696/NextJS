b8bc84eb3a7380787e7bae919a720cd49c963419
Ahmed Ali <ahmedali5696@outlook.com>
2022-12-15 18:41:35 +0200

    .

--- components/sections/ProjectCard.js
+++ components/sections/ProjectCard.js
@@ -7,17 +7,19 @@ export default function ProjectCard({ img, heading, desc, code, live }) {
   return (
     <div className=" min-h-[180px] rounded-md relative overflow-hidden group shadow-md">
       <div className="w-full h-full">
-        <Image className="rounded-md" src={img} width={500} height={500} style={{width: "100%", height: "100%"}} alt="Project Image" />
+        <Image className="rounded-md" src={img} width={500} height={500} style={{ width: "100%", height: "100%" }} alt="Project Image" />
       </div>
       <div className=" -translate-x-full group-hover:translate-x-0 transition-all duration-300 absolute flex flex-col top-0 w-full h-full p-3 bg-[rgb(22,163,74,0.8)] rounded-md text-slate-50">
-        <h4 className=" font-semibold text-lg border-b-2 border-white inline-block mb-1">{heading}</h4>
+        <div>
+          <h4 className=" font-semibold text-lg border-b-2 border-white inline-block mb-1">{heading}</h4>
+        </div>
         <p className="text-gray-200 font-light">{desc}</p>
         <div className="flex flex-col mt-auto">
-          <a className=" mt-auto" href={live} target="_blank" rel="noreferrer">
+          <a href={live} target="_blank" rel="noreferrer">
             View
             <FontAwesomeIcon className="ml-2" size="sm" icon={faArrowRightLong} />
           </a>
-          <a className=" mt-auto" href={code} target="_blank" rel="noreferrer">
+          <a href={code} target="_blank" rel="noreferrer">
             Source
             <FontAwesomeIcon className="ml-2" size="sm" icon={faCode} />
           </a>
